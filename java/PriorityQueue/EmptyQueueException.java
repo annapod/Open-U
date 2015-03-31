997de269d6585897d20821db32825694cf22acc9
annapod <anna.prsz@gmail.com>
2015-03-31 13:04:43 +0300

    Create EmptyQueueException.java

---
+++ java/PriorityQueue/EmptyQueueException.java
@@ -0,0 +1,24 @@
+/**
+ * Anna Podolny 322152893
+ */
+
+/**
+ * @author apodolny
+ *
+ */
+
+public class EmptyQueueException extends Exception{
+	
+	
+	public EmptyQueueException(){
+		super();
+	}
+	
+	public EmptyQueueException(String msg)
+	{
+		super(msg);
+	}
+	
+	
+
+}
