53e5f4373585574e928d38e2c3c17b380495d121
annapod <anna.prsz@gmail.com>
2015-03-31 12:48:15 +0300

    Create ClientManager.java

---
+++ java/chat/ClientManager.java
@@ -0,0 +1,21 @@
+/**
+ * Anna Podolny 322152893
+ */
+package chat;
+
+/**
+ * @author apodolny
+ *
+ */
+public class ClentManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClientGUI g = new ClientGUI();
+
+	}
+
+}
