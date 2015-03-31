86ffe4b9fe718cfbafb67154120e62a1c52dd716
annapod <anna.prsz@gmail.com>
2015-03-31 12:55:22 +0300

    Create Main.java

---
+++ java/threads/semaphore/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Anna Podolny 322152893
+ */
+
+/**
+ * @author apodolny
+ *
+ */
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	/*create junction of 4 semaphores and start tread*/
+	
+	public static void main(String[] args) {
+		
+		Junction j = new Junction(4);
+		j.start();
+		
+
+		
+
+	}
+
+}
