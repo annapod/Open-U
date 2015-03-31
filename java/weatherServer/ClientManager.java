caeb9d2512da6ef965b13c6941a93c1fda397a3e
annapod <anna.prsz@gmail.com>
2015-03-31 12:41:02 +0300

    Create ClientManager.java

---
+++ java/weatherServer/ClientManager.java
@@ -0,0 +1,25 @@
+/**
+ * Anna Podolny 322152893
+ */
+package weatherServer;
+
+
+
+/**
+ * @author apodolny
+ *
+ */
+public class ClientManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ClientWindow w = new ClientWindow();
+		
+		
+
+	}
+
+}
